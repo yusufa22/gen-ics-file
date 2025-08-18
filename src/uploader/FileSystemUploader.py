07bd10b817856cf5ef1fb7d0a720bdd2d8ac50b0
ayub@AyubHP <ayubabdikarim@protonmail.com>
2025-08-18 12:04:08 +0100

    correct mistake in fsuploader

--- src/uploader/FileSystemUploader.py
+++ src/uploader/FileSystemUploader.py
@@ -1,6 +1,6 @@
 from .Uploader import *
 
-class NetlifyUploader():
+class FileSystemUploader():
     def upload(self, calendar):
         open('./generated.ics', 'w').writelines(calendar.serialize_iter())
         print("SUCCESS: ICS file has been generated in this directory with the filename 'generated.ics'")
--- src/uploader/NetlifyUploader.py
+++ src/uploader/NetlifyUploader.py
@@ -2,7 +2,8 @@ from .Uploader import *
 
 class NetlifyUploader():
     def upload(self, calendar):
-      pass
+      open('./generated.ics', 'w').writelines(calendar.serialize_iter())
+      print("SUCCESS: ICS file has been generated in this directory with the filename 'generated.ics'")
 
 #create new netlify site 
 #create new netlify deploy 
